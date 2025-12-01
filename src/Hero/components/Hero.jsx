9e7abbecdd6432e1f6853dab37b58f819bf70a61
Antonio-Benavente <antoniobenavente81@gmail.com>
2025-12-01 00:12:31 -0500

    fix: modificacion de los roles en el hero

--- src/Hero/components/Hero.jsx
+++ src/Hero/components/Hero.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import '../styles/Hero.css'
-import pdf from "../../assets/AntonioBenaventeCV.pdf"; 
+import pdf from "../../assets/AntonioBenaventeCV.pdf";
 
 
 export function Hero({ isDarkMode }) {
-    const roles = ['Frontend Developer', 'UI/UX Enthusiast', 'Problem Solver']
+    const roles = ['Fullstack Developer', 'UI/UX Enthusiast', 'Code Designer', 'Problem Solver']
     const [currentRole, setCurrentRole] = useState(0)
 
     useEffect(() => {
@@ -66,7 +66,7 @@ export function Hero({ isDarkMode }) {
                         </a>
 
                         <a href={pdf} download="AntonioBenaventeCV.pdf" target='_blank' rel="noopener noreferrer" className="social-link cv">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.25} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-file-cv"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" /><path d="M13 11l1.5 6l1.5 -6" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.25} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-file-cv"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" /><path d="M13 11l1.5 6l1.5 -6" /></svg>
                             <span className="social-text">Descargar CV</span>
                         </a>
                     </div>
